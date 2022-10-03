1befdf1150ebf91e367c51239189b41f0088f733
timflee <timflee@hotmail.com>
2022-10-03 21:53:43 +1100

    Python update to include gameoflife attempts and got locked into python and can't covert to blocks!!

--- main.py
+++ main.py
@@ -1,24 +1,150 @@
+def refreshColours():
+    for X2 in range(8):
+        for Y2 in range(8):
+            tileDisplay.set_matrix_color(X2,
+                Y2,
+                Kitronik_Zip_Tile.rgb(list2[X2][Y2][0], list2[X2][Y2][1], list2[X2][Y2][2]))
+        tileDisplay.show()
+def gameOfLife():
+    global Value, Left, Right, Up, Down
+    for X in range(8):
+        if X == 0 or X == 7:
+            continue
+        for Y in range(8):
+            if Y == 0 or Y == 7:
+                continue
+            for rgb in range(3):
+                Value = list2[X][Y][rgb]
+                Left = list2[X - 1][Y][rgb]
+                Right = list2[X + 1][Y][rgb]
+                Up = list2[X][Y + 1][rgb]
+                Down = list2[X][Y - 1][rgb]
+                Value = (Value + Left + Right + Up + Down) / 7
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                list2[X][Y][rgb] = Value
+    refreshColours()
+# Adjust the RGB colour by and random amount
+def randomWalk():
+    global Value
+    for X3 in range(8):
+        for Y3 in range(8):
+            for rgb2 in range(3):
+                Value = list2[X3][Y3][rgb2]
+                Value = Value + randint(-10, 10)
+                if Down > 255:
+                    Value = 255
+                elif Value < 0:
+                    Value = 0
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                list2[X3][Y3][rgb2] = Value
+    refreshColours()
+
 def on_button_pressed_a():
-    list2[0][0][0]=list2[0][0][0] + 5
+    list2[0][0][0] = list2[0][0][0] + 5
     basic.pause(100)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def setToRandomColours():
-    pass
+def gameOfLife2():
+    global numNeighbours, Left, Right, Up, Down, Value
+    for X4 in range(8):
+        if X4 == 0 or X4 == 7:
+            continue
+        for Y4 in range(8):
+            if Y4 == 0 or Y4 == 7:
+                continue
+            for rgb3 in range(2):
+                numNeighbours = 0
+                if list2[X4 - 1][Y4][rgb3] < 125:
+                    numNeighbours += 1
+                if list2[X4 + 1][Y4][rgb3] > 125:
+                    numNeighbours += 1
+                if list2[X4][Y4 + 1][rgb3] > 125:
+                    numNeighbours += 1
+                if list2[X4][Y4 + 1][rgb3] > 125:
+                    numNeighbours += 1
+                if list2[X4][Y4][rgb3] > 125:
+                    numNeighbours += 1
+                Left = 0
+                Right = 0
+                Up = 0
+                Down = list2[X4][Y4 - 1][rgb3]
+                Value = (Value + Left + Right + Up + Down) / 7
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                list2[X4][Y4][rgb3] = Value
+    refreshColours()
+numNeighbours = 0
+Down = 0
+Up = 0
+Right = 0
+Left = 0
+Value = 0
+list2: List[List[List[number]]] = []
+tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = None
 tileDisplay = Kitronik_Zip_Tile.create_zip_tile_display(1, 1, Kitronik_Zip_Tile.UBitLocations.HIDDEN)
 tileDisplay.clear()
-tileDisplay.set_brightness(25)
+brightness = 50
+maxRed = 255
+maxGreen = 1
+maxBlue = 1
+tileDisplay.set_brightness(brightness)
 tileDisplay.show()
-list2 = [[[randint(0, 255), randint(0, 255), randint(0, 255)],
-        [randint(0, 255), randint(0, 255), randint(0, 255)],
-        [randint(0, 255), randint(0, 255), randint(0, 255)],
-        [randint(0, 255), randint(0, 255), randint(0, 255)]]]
-for index in range(3):
-    list2.append([[randint(0, 255), randint(0, 255), randint(0, 255)],
-            [randint(0, 255), randint(0, 255), randint(0, 255)],
-            [randint(0, 255), randint(0, 255), randint(0, 255)],
-            [randint(0, 255), randint(0, 255), randint(0, 255)]])
+basic.pause(1000)
+list2 = [[[randint(0, maxRed),
+            randint(0, maxGreen),
+            randint(0, maxBlue)],
+        [randint(0, maxRed),
+            randint(0, maxGreen),
+            randint(0, maxBlue)],
+        [randint(0, maxRed),
+            randint(0, maxGreen),
+            randint(0, maxBlue)],
+        [randint(0, maxRed),
+            randint(0, maxGreen),
+            randint(0, maxBlue)],
+        [randint(0, maxRed),
+            randint(0, maxGreen),
+            randint(0, maxBlue)],
+        [randint(0, maxRed),
+            randint(0, maxGreen),
+            randint(0, maxBlue)],
+        [randint(0, maxRed),
+            randint(0, maxGreen),
+            randint(0, maxBlue)],
+        [randint(0, maxRed),
+            randint(0, maxGreen),
+            randint(0, maxBlue)]]]
+for index in range(7):
+    list2.append([[randint(0, maxRed),
+                randint(0, maxGreen),
+                randint(0, maxBlue)],
+            [randint(0, maxRed),
+                randint(0, maxGreen),
+                randint(0, maxBlue)],
+            [randint(0, maxRed),
+                randint(0, maxGreen),
+                randint(0, maxBlue)],
+            [randint(0, maxRed),
+                randint(0, maxGreen),
+                randint(0, maxBlue)],
+            [randint(0, maxRed),
+                randint(0, maxGreen),
+                randint(0, maxBlue)],
+            [randint(0, maxRed),
+                randint(0, maxGreen),
+                randint(0, maxBlue)],
+            [randint(0, maxRed),
+                randint(0, maxGreen),
+                randint(0, maxBlue)],
+            [randint(0, maxRed),
+                randint(0, maxGreen),
+                randint(0, maxBlue)]])
 
 def on_forever():
-    setToRandomColours()
+    refreshColours()
+    while True:
+        gameOfLife2()
+        basic.pause(10)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,29 +1,140 @@
-input.onButtonPressed(Button.A, function () {
+function refreshColours() {
+    for (let X2 = 0; X2 < 8; X2++) {
+        for (let Y2 = 0; Y2 < 8; Y2++) {
+            tileDisplay.setMatrixColor(X2, Y2, Kitronik_Zip_Tile.rgb(list2[X2][Y2][0], list2[X2][Y2][1], list2[X2][Y2][2]))
+        }
+        tileDisplay.show()
+    }
+}
+
+function gameOfLife() {
+    
+    for (let X = 0; X < 8; X++) {
+        if (X == 0 || X == 7) {
+            continue
+        }
+        
+        for (let Y = 0; Y < 8; Y++) {
+            if (Y == 0 || Y == 7) {
+                continue
+            }
+            
+            for (let rgb = 0; rgb < 3; rgb++) {
+                Value = list2[X][Y][rgb]
+                Left = list2[X - 1][Y][rgb]
+                Right = list2[X + 1][Y][rgb]
+                Up = list2[X][Y + 1][rgb]
+                Down = list2[X][Y - 1][rgb]
+                Value = (Value + Left + Right + Up + Down) / 7
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                list2[X][Y][rgb] = Value
+            }
+        }
+    }
+    refreshColours()
+}
+
+//  Adjust the RGB colour by and random amount
+function randomWalk() {
+    
+    for (let X3 = 0; X3 < 8; X3++) {
+        for (let Y3 = 0; Y3 < 8; Y3++) {
+            for (let rgb2 = 0; rgb2 < 3; rgb2++) {
+                Value = list2[X3][Y3][rgb2]
+                Value = Value + randint(-10, 10)
+                if (Down > 255) {
+                    Value = 255
+                } else if (Value < 0) {
+                    Value = 0
+                }
+                
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                list2[X3][Y3][rgb2] = Value
+            }
+        }
+    }
+    refreshColours()
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     list2[0][0][0] = list2[0][0][0] + 5
     basic.pause(100)
 })
-function setToRandomColours () {
-	
+function gameOfLife2() {
+    
+    for (let X4 = 0; X4 < 8; X4++) {
+        if (X4 == 0 || X4 == 7) {
+            continue
+        }
+        
+        for (let Y4 = 0; Y4 < 8; Y4++) {
+            if (Y4 == 0 || Y4 == 7) {
+                continue
+            }
+            
+            for (let rgb3 = 0; rgb3 < 2; rgb3++) {
+                numNeighbours = 0
+                if (list2[X4 - 1][Y4][rgb3] < 125) {
+                    numNeighbours += 1
+                }
+                
+                if (list2[X4 + 1][Y4][rgb3] > 125) {
+                    numNeighbours += 1
+                }
+                
+                if (list2[X4][Y4 + 1][rgb3] > 125) {
+                    numNeighbours += 1
+                }
+                
+                if (list2[X4][Y4 + 1][rgb3] > 125) {
+                    numNeighbours += 1
+                }
+                
+                if (list2[X4][Y4][rgb3] > 125) {
+                    numNeighbours += 1
+                }
+                
+                Left = 0
+                Right = 0
+                Up = 0
+                Down = list2[X4][Y4 - 1][rgb3]
+                Value = (Value + Left + Right + Up + Down) / 7
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                list2[X4][Y4][rgb3] = Value
+            }
+        }
+    }
+    refreshColours()
 }
-let list2: number[][][] = []
-let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
+
+let numNeighbours = 0
+let Down = 0
+let Up = 0
+let Right = 0
+let Left = 0
+let Value = 0
+let list2 : number[][][] = []
+let tileDisplay : Kitronik_Zip_Tile.ZIPTileDisplay = null
+tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
 tileDisplay.clear()
-tileDisplay.setBrightness(25)
+let brightness = 50
+let maxRed = 255
+let maxGreen = 1
+let maxBlue = 1
+tileDisplay.setBrightness(brightness)
 tileDisplay.show()
-list2 = [[
-[randint(0, 255), randint(0, 255), randint(0, 255)],
-[randint(0, 255), randint(0, 255), randint(0, 255)],
-[randint(0, 255), randint(0, 255), randint(0, 255)],
-[randint(0, 255), randint(0, 255), randint(0, 255)]
-]]
-for (let index = 0; index < 3; index++) {
-    list2.push([
-    [randint(0, 255), randint(0, 255), randint(0, 255)],
-    [randint(0, 255), randint(0, 255), randint(0, 255)],
-    [randint(0, 255), randint(0, 255), randint(0, 255)],
-    [randint(0, 255), randint(0, 255), randint(0, 255)]
-    ])
+basic.pause(1000)
+list2 = [[[randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)]]]
+for (let index = 0; index < 7; index++) {
+    list2.push([[randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)], [randint(0, maxRed), randint(0, maxGreen), randint(0, maxBlue)]])
 }
-basic.forever(function () {
-    setToRandomColours()
+basic.forever(function on_forever() {
+    refreshColours()
+    while (true) {
+        gameOfLife2()
+        basic.pause(10)
+    }
 })
