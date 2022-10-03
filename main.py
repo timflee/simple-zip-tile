53873b63e7b6502818d44ba0a233ea8b4a5629d6
timflee <timflee@hotmail.com>
2022-10-03 22:36:20 +1100

    Working game of life with 3 colours

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ def gameOfLife():
                 Value = (Value + Left + Right + Up + Down) / 7
                 # Update the RGB value in the reference array
                 # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
                 list2[X][Y][rgb] = Value
     refreshColours()
 # Adjust the RGB colour by and random amount
@@ -38,42 +44,56 @@ def randomWalk():
                     Value = 0
                 # Update the RGB value in the reference array
                 # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
                 list2[X3][Y3][rgb2] = Value
     refreshColours()
-
-def on_button_pressed_a():
-    list2[0][0][0] = list2[0][0][0] + 5
-    basic.pause(100)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def gameOfLife2():
-    global numNeighbours, Left, Right, Up, Down, Value
+def binarySplit(threshold: number, max2: number):
+    global Value
+    for X32 in range(8):
+        for Y32 in range(8):
+            for rgb22 in range(3):
+                Value = list2[X32][Y32][rgb22]
+                if Value > threshold:
+                    Value = max2
+                else:
+                    Value = 0
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                # Update the RGB value in the reference array
+                list2[X32][Y32][rgb22] = Value
+    refreshColours()
+def gameOfLife2(threshold2: number, max3: number):
+    global numNeighbours
     for X4 in range(8):
-        if X4 == 0 or X4 == 7:
-            continue
         for Y4 in range(8):
-            if Y4 == 0 or Y4 == 7:
-                continue
-            for rgb3 in range(2):
+            for rgb3 in range(3):
                 numNeighbours = 0
-                if list2[X4 - 1][Y4][rgb3] < 125:
-                    numNeighbours += 1
-                if list2[X4 + 1][Y4][rgb3] > 125:
-                    numNeighbours += 1
-                if list2[X4][Y4 + 1][rgb3] > 125:
-                    numNeighbours += 1
-                if list2[X4][Y4 + 1][rgb3] > 125:
-                    numNeighbours += 1
-                if list2[X4][Y4][rgb3] > 125:
-                    numNeighbours += 1
-                Left = 0
-                Right = 0
-                Up = 0
-                Down = list2[X4][Y4 - 1][rgb3]
-                Value = (Value + Left + Right + Up + Down) / 7
-                # Update the RGB value in the reference array
-                # Update the RGB value in the reference array
-                list2[X4][Y4][rgb3] = Value
+                if X4 > 0:
+                    if list2[X4 - 1][Y4][rgb3] > threshold2:
+                        numNeighbours += 1
+                if X4 < 7:
+                    if list2[X4 + 1][Y4][rgb3] > threshold2:
+                        numNeighbours += 1
+                if Y4 > 0:
+                    if list2[X4][Y4 - 1][rgb3] > threshold2:
+                        numNeighbours += 1
+                if Y4 < 7:
+                    if list2[X4][Y4 + 1][rgb3] > threshold2:
+                        numNeighbours += 1
+                if list2[X4][Y4][rgb3] >= threshold2 and (numNeighbours < 2 or numNeighbours > 3):
+                    list2[X4][Y4][rgb3] = 0
+                if list2[X4][Y4][rgb3] < threshold2 and (numNeighbours == 2 or numNeighbours == 3):
+                    list2[X4][Y4][rgb3] = max3
     refreshColours()
 numNeighbours = 0
 Down = 0
@@ -85,10 +105,10 @@ list2: List[List[List[number]]] = []
 tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = None
 tileDisplay = Kitronik_Zip_Tile.create_zip_tile_display(1, 1, Kitronik_Zip_Tile.UBitLocations.HIDDEN)
 tileDisplay.clear()
-brightness = 50
+brightness = 25
 maxRed = 255
-maxGreen = 1
-maxBlue = 1
+maxGreen = 255
+maxBlue = 255
 tileDisplay.set_brightness(brightness)
 tileDisplay.show()
 basic.pause(1000)
@@ -144,7 +164,10 @@ for index in range(7):
 
 def on_forever():
     refreshColours()
+    basic.pause(200)
+    binarySplit(maxRed / 2, maxRed)
+    basic.pause(200)
     while True:
-        gameOfLife2()
-        basic.pause(10)
+        gameOfLife2(maxRed / 1.5, maxRed)
+        basic.pause(5)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -28,6 +28,12 @@ function gameOfLife() {
                 Value = (Value + Left + Right + Up + Down) / 7
                 //  Update the RGB value in the reference array
                 //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
                 list2[X][Y][rgb] = Value
             }
         }
@@ -51,6 +57,12 @@ function randomWalk() {
                 
                 //  Update the RGB value in the reference array
                 //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
                 list2[X3][Y3][rgb2] = Value
             }
         }
@@ -58,52 +70,75 @@ function randomWalk() {
     refreshColours()
 }
 
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    list2[0][0][0] = list2[0][0][0] + 5
-    basic.pause(100)
-})
-function gameOfLife2() {
+function binarySplit(threshold: number, max2: number) {
+    
+    for (let X32 = 0; X32 < 8; X32++) {
+        for (let Y32 = 0; Y32 < 8; Y32++) {
+            for (let rgb22 = 0; rgb22 < 3; rgb22++) {
+                Value = list2[X32][Y32][rgb22]
+                if (Value > threshold) {
+                    Value = max2
+                } else {
+                    Value = 0
+                }
+                
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                //  Update the RGB value in the reference array
+                list2[X32][Y32][rgb22] = Value
+            }
+        }
+    }
+    refreshColours()
+}
+
+function gameOfLife2(threshold2: number, max3: number) {
     
     for (let X4 = 0; X4 < 8; X4++) {
-        if (X4 == 0 || X4 == 7) {
-            continue
-        }
-        
         for (let Y4 = 0; Y4 < 8; Y4++) {
-            if (Y4 == 0 || Y4 == 7) {
-                continue
-            }
-            
-            for (let rgb3 = 0; rgb3 < 2; rgb3++) {
+            for (let rgb3 = 0; rgb3 < 3; rgb3++) {
                 numNeighbours = 0
-                if (list2[X4 - 1][Y4][rgb3] < 125) {
-                    numNeighbours += 1
+                if (X4 > 0) {
+                    if (list2[X4 - 1][Y4][rgb3] > threshold2) {
+                        numNeighbours += 1
+                    }
+                    
                 }
                 
-                if (list2[X4 + 1][Y4][rgb3] > 125) {
-                    numNeighbours += 1
+                if (X4 < 7) {
+                    if (list2[X4 + 1][Y4][rgb3] > threshold2) {
+                        numNeighbours += 1
+                    }
+                    
                 }
                 
-                if (list2[X4][Y4 + 1][rgb3] > 125) {
-                    numNeighbours += 1
+                if (Y4 > 0) {
+                    if (list2[X4][Y4 - 1][rgb3] > threshold2) {
+                        numNeighbours += 1
+                    }
+                    
                 }
                 
-                if (list2[X4][Y4 + 1][rgb3] > 125) {
-                    numNeighbours += 1
+                if (Y4 < 7) {
+                    if (list2[X4][Y4 + 1][rgb3] > threshold2) {
+                        numNeighbours += 1
+                    }
+                    
                 }
                 
-                if (list2[X4][Y4][rgb3] > 125) {
-                    numNeighbours += 1
+                if (list2[X4][Y4][rgb3] >= threshold2 && (numNeighbours < 2 || numNeighbours > 3)) {
+                    list2[X4][Y4][rgb3] = 0
                 }
                 
-                Left = 0
-                Right = 0
-                Up = 0
-                Down = list2[X4][Y4 - 1][rgb3]
-                Value = (Value + Left + Right + Up + Down) / 7
-                //  Update the RGB value in the reference array
-                //  Update the RGB value in the reference array
-                list2[X4][Y4][rgb3] = Value
+                if (list2[X4][Y4][rgb3] < threshold2 && (numNeighbours == 2 || numNeighbours == 3)) {
+                    list2[X4][Y4][rgb3] = max3
+                }
+                
             }
         }
     }
@@ -120,10 +155,10 @@ let list2 : number[][][] = []
 let tileDisplay : Kitronik_Zip_Tile.ZIPTileDisplay = null
 tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
 tileDisplay.clear()
-let brightness = 50
+let brightness = 25
 let maxRed = 255
-let maxGreen = 1
-let maxBlue = 1
+let maxGreen = 255
+let maxBlue = 255
 tileDisplay.setBrightness(brightness)
 tileDisplay.show()
 basic.pause(1000)
@@ -133,8 +168,11 @@ for (let index = 0; index < 7; index++) {
 }
 basic.forever(function on_forever() {
     refreshColours()
+    basic.pause(200)
+    binarySplit(maxRed / 2, maxRed)
+    basic.pause(200)
     while (true) {
-        gameOfLife2()
-        basic.pause(10)
+        gameOfLife2(maxRed / 1.5, maxRed)
+        basic.pause(5)
     }
 })
